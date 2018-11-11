4f88fb7f96b10a6de915a4abdafc10d4b1ffa752
Matthew Campbell <cattmampbell@gmail.com>
2018-11-11 14:34:17 -0500

    Completed Inheritance tasks, applied further styling to console logs.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+/* More on Colorful Console Messages here: https://www.samanthaming.com/tidbits/40-colorful-console-message */
+
 // console.log(`Testing... Testing... 1, 2, 3, 4, OOP Drills.`);
 console.log('%cStart%c of scripts.js file!', 'font-weight: 900; color: blue;', 'font-weight: 400; color: blue;'); // Should log: Start of scripts.js file!
 // console.log(`%c... ... ...`, 'color: grey;');
@@ -9,39 +11,39 @@ console.log('%cStart%c of scripts.js file!', 'font-weight: 900; color: blue;', '
 const person1 = {
     name: `Olivia`,
     sayHello: function() {
-        console.log(`Hello! My name is ${this.name}.`, `color: red;`)
+        console.log(`   Hello! My name is ${this.name}.`, `color: red;`)
     }
 }
 
 const person2 = {
     name: `Toast`,
     sayHello: function() {
-        console.log(`Hello! My name is ${this.name}.`)
+        console.log(`   Hello! My name is ${this.name}.`)
     }
 }
 
 const person3 = {
     name: `Caleb`,
     sayHello: function() {
-        console.log(`Hello! My name is ${this.name}.`)
+        console.log(`   Hello! My name is ${this.name}.`)
     }
 }
 
 const person4 = {
     name: `Cody`,
     sayHello: function() {
-        console.log(`Hello! My name is ${this.name}.`)
+        console.log(`   Hello! My name is ${this.name}.`)
     }
 }
 
 const person5 = {
     name: `Megan`,
     sayHello: function() {
-        console.log(`Hello! My name is ${this.name}.`)
+        console.log(`   Hello! My name is ${this.name}.`)
     }
 }
 
-console.log(`%cVia %cObject Literal%c Syntax:`, `font-weight: 600; color: black;`, `font-weight: 900; text-decoration: underline; color: green;`, `font-weight: 600; color: black;`)
+console.log(`%cVia %cObject Literal%c Syntax:`, `font-weight: 600; color: black;`, `font-weight: 900; text-decoration: underline; color: green;`, `font-weight: 600; color: black;`);
 
 // Call the sayHello() function on each object
 person1.sayHello(); // Should log: Hello! My name is Olivia.
@@ -62,7 +64,7 @@ function Person(name, age, city) {
 // Create a method that will log a string a string to the console like this:
 // "Hey! My name is ___. I am ___ years old and I live in ___."
 Person.prototype.sayHello = function() {
-    console.log('Hey! My name is ' + this.name + '. I am ' + this.age + ' and I live in ' + this.city + '.');
+    console.log('   Hey! My name is ' + this.name + '. I am ' + this.age + ' and I live in ' + this.city + '.');
 }
 
 // Create 5 instances of the same friends from before, using the pseudo-class and method created above
@@ -72,7 +74,7 @@ const person3Again = new Person(`Caleb`, 25, `Kennesaw`);
 const person4Again = new Person(`Cody`, 28, `Red Bank`);
 const person5Again = new Person(`Megan`, 30, `Red Bank`);
 
-console.log(`%cVia %cES5%c Syntax:`, `font-weight: 600; color: black;`, `font-weight: 900; text-decoration: underline; color: green;`, `font-weight: 600; color: black;`)
+console.log(`%cVia %cES5%c Syntax:`, `font-weight: 600; color: black;`, `font-weight: 900; text-decoration: underline; color: green;`, `font-weight: 600; color: black;`);
 
 // Call the sayHello() function on each object, and check the console 
 person1Again.sayHello(); // Should log: Hey! My name is Olivia. I am 24 years old and I live in Hixson.
@@ -92,7 +94,7 @@ class PersonAgain {
     }
 
     sayHelloAgain() {
-        console.log(`Hey! My name is ${this.name}. I am ${this.age} and I live in ${this.city}.`)
+        console.log(`   Hey! My name is ${this.name}. I am ${this.age} and I live in ${this.city}.`)
     }
 }
 // Create 5 instances of the same friends from before, using ES6/ES2015 syntax
@@ -102,7 +104,7 @@ const person3ES6 = new PersonAgain(`Caleb`, 25, `Kennesaw`);
 const person4ES6 = new PersonAgain(`Cody`, 28, `Red Bank`);
 const person5ES6 = new PersonAgain(`Megan`, 30, `Red Bank`);
 
-console.log(`%cVia %cES6%c Syntax:`, `font-weight: 600; color: black;`, `font-weight: 900; text-decoration: underline; color: green;`, `font-weight: 600; color: black;`)
+console.log(`%cVia %cES6%c Syntax:`, `font-weight: 600; color: black;`, `font-weight: 900; text-decoration: underline; color: green;`, `font-weight: 600; color: black;`);
 
 // Call the sayHello() function on each object, and check the console 
 person1ES6.sayHelloAgain(); // Should log: Hey! My name is Olivia. I am 24 years old and I live in Hixson.
@@ -111,9 +113,9 @@ person3ES6.sayHelloAgain(); // Should log: Hey! My name is Caleb. I am 25 years 
 person4ES6.sayHelloAgain(); // Should log: Hey! My name is Cody. I am 28 years old and I live in Red Bank.
 person5ES6.sayHelloAgain(); // Should log: Hey! My name is Megan. I am 30 years old and I live in Red Bank.
 
-console.log(`%c... ... ...`, 'color: grey;');
+console.log(`%c. . .`, 'color: grey; font-weight: 800; font-size: 12px;');
 
-// Create a parent class called 'Vehicle', with properties for the type (String - 'truck, 'sedan', 'motorcycle' or 'coupe'), manufacturer (String) and the number of wheels (Number)
+// Create a parent class called 'Vehicle', with properties for the type (String - 'Truck, 'Sedan', 'Motorcycle', 'Coupe', etc.), manufacturer (String) and the number of wheels (Number)
 class Vehicle {
     constructor(type, manufacturer, model, numberOfWheels) {
         this.type = type;
@@ -124,6 +126,7 @@ class Vehicle {
 
     // Create a method called 'aboutVehicle' so that it logs a String to the console  
     aboutVehicle() {
+        console.log(`%cVia %cVehicle%c Parent Class:`, `font-weight: 600;`, `font-weight: 900; text-decoration: underline;`, `font-weight: 600; color:`);
         console.log(`   %cVehicle Type:%c ${this.type}`, `text-decoration: underline; font-weight: 600;`, `color: green; font-weight: 600;`);
         console.log(`   %cManufacturer:%c ${this.manufacturer}`, `text-decoration: underline;`, `font-weight: 400;`);
         console.log(`   %cModel:%c ${this.model}`, `text-decoration: underline;`, `font-weight: 400;`);
@@ -133,7 +136,7 @@ class Vehicle {
 
 // Create a child class called 'Truck' with additional properties for number of doors (Number) and truck bed (Boolean)
 class Truck extends Vehicle {
-    constructor(numberOfDoors, truckBed) {
+    constructor(type, manufacturer, model, numberOfWheels, numberOfDoors, truckBed) {
         super(type, manufacturer, model, numberOfWheels);
         this.numberOfDoors = numberOfDoors;
         this.truckBed = truckBed;
@@ -141,18 +144,19 @@ class Truck extends Vehicle {
 
     // Overwrite the inherited method called 'aboutVehicle' so that it logs a String to the console 
     aboutVehicle() {
+        console.log(`%cVia %cTruck%c Child Class:`, `font-weight: 600;`, `font-weight: 900; text-decoration: underline;`, `font-weight: 600; color:`);
         console.log(`   %cVehicle Type:%c ${this.type}`, `text-decoration: underline; font-weight: 600;`, `color: green; font-weight: 600;`);
         console.log(`   %cManufacturer:%c ${this.manufacturer}`, `text-decoration: underline;`, `font-weight: 400;`);
         console.log(`   %cModel:%c ${this.model}`, `text-decoration: underline;`, `font-weight: 400;`);
         console.log(`   %cWheels:%c ${this.numberOfWheels}`, `text-decoration: underline;`, `font-weight: 400;`);
-        console.log(`       %cDoors:%c ${this.numberOfDoors}`, `text-decoration: underline; color: grey;`, `font-weight: 400;`);
-        console.log(`       %cHas Truck Bed:%c ${this.truckBed}`, `text-decoration: underline; color: grey;`, `font-weight: 400;`);
+        console.log(`       %cDoors:%c ${this.numberOfDoors}`, `font-weight: 700; text-decoration: underline; color: grey;`, `font-weight: 400;`);
+        console.log(`       %cHas Truck Bed:%c ${this.truckBed}`, `font-weight: 700; text-decoration: underline; color: grey;`, `font-weight: 400;`);
     }
 }
 
-// Create a child class called 'Sedan' with additional properties for size (String - 'small' or 'medium'), and miles per gallon (Number)
+// Create a child class called 'Sedan' with additional properties for size (String - 'Small' or 'Medium'), and miles per gallon (Number)
 class Sedan extends Vehicle {
-    constructor(size, milesPerGallon) {
+    constructor(type, manufacturer, model, numberOfWheels, size, milesPerGallon) {
         super(type, manufacturer, model, numberOfWheels);
         this.size = size;
         this.milesPerGallon = milesPerGallon;
@@ -160,43 +164,55 @@ class Sedan extends Vehicle {
 
     // Overwrite the inherited method called 'aboutVehicle' so that it logs a String to the console 
     aboutVehicle() {
+        console.log(`%cVia %cSedan%c Child Class:`, `font-weight: 600;`, `font-weight: 900; text-decoration: underline;`, `font-weight: 600; color:`);
         console.log(`   %cVehicle Type:%c ${this.type}`, `text-decoration: underline; font-weight: 600;`, `color: green; font-weight: 600;`);
         console.log(`   %cManufacturer:%c ${this.manufacturer}`, `text-decoration: underline;`, `font-weight: 400;`);
         console.log(`   %cModel:%c ${this.model}`, `text-decoration: underline;`, `font-weight: 400;`);
         console.log(`   %cWheels:%c ${this.numberOfWheels}`, `text-decoration: underline;`, `font-weight: 400;`);
-        console.log(`       %cSize:%c ${this.size}`, `text-decoration: underline; color: grey;`, `font-weight: 400;`);
-        console.log(`       %cMiles per Gallon (MPG):%c ${this.milesPerGallon}`, `text-decoration: underline; color: grey;`, `font-weight: 400;`);
+        console.log(`       %cSize:%c ${this.size}`, `font-weight: 700; text-decoration: underline; color: grey;`, `font-weight: 400;`);
+        console.log(`       %cMiles per Gallon (MPG):%c ${this.milesPerGallon}`, `font-weight: 700; text-decoration: underline; color: grey;`, `font-weight: 400;`);
     }
 }
 
 // Create a child class called 'Motorcycle' with an additional property to determine that is has handlebars (Boolean) as well as another property to express that it has no doors (Boolean)
 class Motorcycle extends Vehicle {
-    constructor(hasHandlebars, hasDoors) {
+    constructor(type, manufacturer, model, numberOfWheels, hasHandlebars, hasDoors) {
         super(type, manufacturer, model, numberOfWheels);
         this.hasHandlebars = hasHandlebars; 
         this.hasDoors = hasDoors;
     }
 
-    // Overwrite the inherited method called 'aboutVehicle' so that it logs a String to the console like this: 
-    // "Vehicle: Type - ___, Manufacturer - ___, Model - ___, Wheels - ___."
-    // "Has Handlebars - ___, Has Doors - ___.'
+    // Overwrite the inherited method called 'aboutVehicle' so that it logs a String to the console
     aboutVehicle() {
+        console.log(`%cVia %cMotorcycle%c Child Class:`, `font-weight: 600;`, `font-weight: 900; text-decoration: underline;`, `font-weight: 600; color:`);
         console.log(`   %cVehicle Type:%c ${this.type}`, `text-decoration: underline; font-weight: 600;`, `color: green; font-weight: 600;`);
         console.log(`   %cManufacturer:%c ${this.manufacturer}`, `text-decoration: underline;`, `font-weight: 400;`);
         console.log(`   %cModel:%c ${this.model}`, `text-decoration: underline;`, `font-weight: 400;`);
         console.log(`   %cWheels:%c ${this.numberOfWheels}`, `text-decoration: underline;`, `font-weight: 400;`);
-        console.log(`       %cHas Handlebars:%c ${this.hasHandlebars}`, `text-decoration: underline; color: grey;`, `font-weight: 400;`); 
-        console.log(`       %cHas Doors:%c ${this.hasDoors}`, `text-decoration: underline; color: grey;`, `font-weight: 400;`); 
+        console.log(`       %cHas Handlebars:%c ${this.hasHandlebars}`, `font-weight: 700; text-decoration: underline; color: grey;`, `font-weight: 400;`); 
+        console.log(`       %cHas Doors:%c ${this.hasDoors}`, `font-weight: 700; text-decoration: underline; color: grey;`, `font-weight: 400;`); 
     }
 }
 
-// Instantiation!!!
-const accent = new Vehicle('Subcompact', 'Hyundai', 'Accent', 4);
-// const ranger = new Truck('Truck', 'Ford', 'Ranger', 4, 2, true);
+// Instantiate 4 objects via the Vehicle parent class, Truck, Sedan and Motorcycle children classes
+let hyundaiAccent = new Vehicle('Subcompact', 'Hyundai', 'Accent', 4);
+let fordRanger = new Truck('Truck', 'Ford', 'Ranger', 4, 2, true);
+let toyotaCamry = new Sedan('Sedan', 'Toyota', 'Camry', 4, 'Medium', 34);
+let onyxCTY = new Motorcycle('Motorcycle', 'Onyx', 'CTY', 2, true, false);
 
-// Call the aboutVehicle() Method on each instantiated object
-accent.aboutVehicle(); 
-// ranger.aboutVehicle(); 
+// Log 4 instantiated objects to the console
+console.log(hyundaiAccent);
+console.log(fordRanger);
+console.log(toyotaCamry);
+console.log(onyxCTY);
 
-console.log(`%c... ... ...`, 'color: grey;');
+console.log(`%c. . .`, 'color: grey; font-weight: 800; font-size: 12px;');
+
+// Call the aboutVehicle() Method on each of the 4 instantiated objects
+hyundaiAccent.aboutVehicle(); 
+fordRanger.aboutVehicle(); 
+toyotaCamry.aboutVehicle();
+onyxCTY.aboutVehicle();
+
+console.log(`%c. . .`, 'color: grey; font-weight: 800; font-size: 12px;');
 console.log('%cEnd%c of scripts.js file!', 'font-weight: 900; color: blue;', 'font-weight: 400; color: blue;'); // Should log: End of scripts.js file!
